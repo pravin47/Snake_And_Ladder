625ae49e27b7491387390caa5f924724f4519625
pravin47 <bagul.pravin2@gmail.com>
2021-02-05 23:12:22 +0530

    Added:Till_Player_Reaches_Winning_100

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -7,26 +7,41 @@ public class Snake_And_Ladder {
 		final int no_play = 0;
 		final int Ladder = 1;
 		final int snake = 2;
-
+		final int Win_position = 100;
+		
 		int position = 0;
 		int no_of_die = 0;
 
-		int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-
-		int option = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (option) {
-		case no_play:
-			position = 0;
-			break;
-		case Ladder:
-			position = die;
-			break;
-		case snake:
-			position = -die;
-			break;
+		while(position != Win_position) 
+		{
+				int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+		
+				int option = (int) (Math.floor(Math.random() * 10) % 3);
+				switch (option) 
+				{
+					case no_play:
+						no_of_die = 0;
+						break;
+					case Ladder:
+						no_of_die = die;
+						break;
+					case snake:
+						no_of_die = -die;
+						break;
+				}
+				
+				position = position + no_of_die;
+				
+						if(position<no_play)
+						{
+							position = no_play;
+						}
+						else if(position>Win_position)
+						{
+							position = position - no_of_die;
+						}
 		}
-		System.out.println("Player Position is :" + position);
+		System.out.println("Player Winning Position is :" +position );
 	}
-
 }
-
+				
