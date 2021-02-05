2a1f1c92b74a189e091f054f97515d3f0cada256
pravin47 <bagul.pravin2@gmail.com>
2021-02-05 23:25:19 +0530

    Added:Ensure_Winning_Position

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -3,45 +3,40 @@ package com.Snake_Ladder;
 public class Snake_And_Ladder {
 
 	public static void main(String[] args) {
-
+		// final declaration
 		final int no_play = 0;
 		final int Ladder = 1;
 		final int snake = 2;
 		final int Win_position = 100;
-		
+		// declaraton
 		int position = 0;
 		int no_of_die = 0;
+		// winnig position till 100
+		while (position != Win_position) {
+			// checkin Dice
+			int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+			// checking Different option
+			int option = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (option) {
+			case no_play:
+				no_of_die = 0;
+				break;
+			case Ladder:
+				no_of_die = die;
+				break;
+			case snake:
+				no_of_die = -die;
+				break;
+			}
 
-		while(position != Win_position) 
-		{
-				int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-		
-				int option = (int) (Math.floor(Math.random() * 10) % 3);
-				switch (option) 
-				{
-					case no_play:
-						no_of_die = 0;
-						break;
-					case Ladder:
-						no_of_die = die;
-						break;
-					case snake:
-						no_of_die = -die;
-						break;
-				}
-				
-				position = position + no_of_die;
-				
-						if(position<no_play)
-						{
-							position = no_play;
-						}
-						else if(position>Win_position)
-						{
-							position = position - no_of_die;
-						}
+			position = position + no_of_die;
+			// checking different position
+			if (position < no_play) {
+				position = no_play;
+			} else if (position > Win_position) {
+				position = position - no_of_die;
+			}
 		}
-		System.out.println("Player Winning Position is :" +position );
+		System.out.println("Player Winning Position is :" + position);
 	}
 }
-				
