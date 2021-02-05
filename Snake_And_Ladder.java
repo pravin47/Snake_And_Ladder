1522f9dcc20e703014fd0f96a61e5fe5ae119089
pravin47 <bagul.pravin2@gmail.com>
2021-02-05 21:51:34 +0530

    Added:Get_Number_Between_1_to_6

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -3,10 +3,10 @@ package com.Snake_Ladder;
 public class Snake_And_Ladder {
 
 	public static void main(String[] args) {
-		
+
 		int pos1 = 0;
-	
+		int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
 	}
-		
+
 }
 
