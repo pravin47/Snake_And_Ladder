c8cac550ef7db9e70ee1e8da54d13dc1e35834f0
pravin47 <bagul.pravin2@gmail.com>
2021-02-05 22:21:33 +0530

    Added:Check_Multiple_Option

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -4,8 +4,28 @@ public class Snake_And_Ladder {
 
 	public static void main(String[] args) {
 
-		int pos1 = 0;
+		final int no_play = 0;
+		final int Ladder = 1;
+		final int snake = 2;
+
+		int position = 0;
+		int no_of_die = 0;
+
 		int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+
+		int option = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (option) {
+		case no_play:
+			position = 0;
+			break;
+		case Ladder:
+			position = die;
+			break;
+		case snake:
+			position = -die;
+			break;
+		}
+		System.out.println("Player Position is :" + position);
 	}
 
 }
